c328df8a4925e795b2580e62ac31bec47fc27262
johannes <johannes@johannes-pc>
2017-07-08 16:51:55 +0200

    back toversion 1.1.3

--- Flink/src/main/java/com/tu/bdap/pagerank/PageRank_Algorithm.java
+++ Flink/src/main/java/com/tu/bdap/pagerank/PageRank_Algorithm.java
@@ -1,45 +1,46 @@
 package com.tu.bdap.pagerank;
 
 import org.apache.flink.api.common.functions.FilterFunction;
+import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
-import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.graph.Edge;
 import org.apache.flink.graph.Graph;
-import org.apache.flink.graph.library.link_analysis.PageRank;
-import org.apache.flink.types.NullValue;
+import org.apache.flink.graph.library.PageRank;
+import org.apache.flink.util.Collector;
 
 public class PageRank_Algorithm {
 	
 	public static void main(String[] args) throws Exception {
 
-        final double DAMPING_FACTOR = 0.15;
-        final int ITERATIONS = 20;
+		// Create execution environment
+		ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
 
-        ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+		DataSet<Edge<Integer, Double>> edges = env.readTextFile("/home/johannes/Dropbox/BDAPRO_Share/Datasets/ny.txt").filter(new FilterFunction<String>() {
+			@Override
+			public boolean filter(String value) throws Exception {
+				return value.startsWith("a");
+			}
+		}).flatMap(new FlatMapFunction<String, Edge<Integer, Double>>() {
+			@Override
+			public void flatMap(String value, Collector<Edge<Integer, Double>> out) throws Exception {
+				String[] values = value.split(" ");
+				out.collect(new Edge<Integer, Double>(Integer.parseInt(values[1]), Integer.parseInt(values[2]),
+						Double.parseDouble(values[3])));
+			}
+		});
 
-        DataSet<Tuple2<String, String>> edges = env.readTextFile(args[0]).filter(new FilterFunction<String>() {
-            @Override
-            public boolean filter(String value) throws Exception {
-                return value.startsWith("a");
-            }
-        }).map(new MapFunction<String, Tuple2<String, String>>() {
-            @Override
-            public Tuple2<String, String> map(String value) throws Exception {
-                String[] values = value.split(" ");
-                return new Tuple2<String, String>(values[1], values[2]);
-            }
-        });
-
-        Graph<String, NullValue,NullValue> graph = Graph.fromTuple2DataSet(edges, env);
-
-        PageRank pageRank = new PageRank(DAMPING_FACTOR, ITERATIONS);
-
-
-        DataSet<PageRank.Result<String>> result = pageRank.runInternal(graph);
-
-        result.print();
-
+		Graph<Integer, Double, Double> graph = Graph.fromDataSet(edges, new MapFunction<Integer, Double>() {
+			@Override
+			public Double map(Integer value) throws Exception {
+				return 1.0;
+			}
+		}, env);
+		
+		//graph.run(new SingleSourceShortestPaths<Integer>(1, 20)).print();;
+		PageRank<Integer> pr = new PageRank<>(0.01, 20);
+		pr.run(graph).print();
     }
 
 }
